54cb570016de452cbd51b3e748f7d5d5c8c095cd
Ivan Karpan <ivan.karpan@gmail.com>
2014-01-03 13:23:35 +0200

    Fixed no new line at end of file issue that could not be worked around with -w flag Made name property of PESGraphEdge strong to avoid broken references

--- PESGraph/PESGraphEdge.h
+++ PESGraph/PESGraphEdge.h
@@ -20,11 +20,11 @@
         An optional description of this edge, such as the road it depicts or
         the airline flight its represents, etc.
      */
-    NSString *__unsafe_unretained name;
+    NSString *name;
 }
 
 @property (nonatomic, strong) NSNumber *weight;
-@property (nonatomic, unsafe_unretained) NSString *name;
+@property (nonatomic, strong) NSString *name;
 
 /**
 	Convenience initializer that allows for setting the edge's name and number in 
